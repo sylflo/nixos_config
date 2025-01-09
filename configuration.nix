457bcec6fec664b177a265f4ae578af2f6e4994f
sylflo <git@sylvain-chateau.com>
2025-01-09 23:19:05 +0100

    starting a new

--- configuration.nix
+++ configuration.nix
@@ -2,29 +2,30 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ inputs, config, pkgs, lib, ... }:
+{ inputs, config, pkgs, ... }:
 
 {
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      #./home-manager-config.nix
     ];
 
-  nix.settings = {
+  nix.settings= {
+    experimental-features = [ "nix-command" "flakes" ];
     substituters = ["https://hyprland.cachix.org"];
     trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
-    # Experimental feature
-    experimental-features = [ "nix-command" "flakes" ];
   };
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
-  #boot.loader.systemd-boot.loader.generations = 2;
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -47,71 +48,19 @@
     LC_TIME = "fr_FR.UTF-8";
   };
 
-  # Enable graphics for steam
-  hardware.graphics.enable = true;
-  #hardware.video.hardenedIntel = false;  # Disable hardened Intel for better performance.
-  #hardware.video.intel.vaapi.enable = true;  # For hardware-accelerated video decoding.
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-
-  # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
   };
-
-  # Enable touchpad support (enabled default in most desktopManager).
-  services.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.sylflo = {
     isNormalUser = true;
     description = "sylflo";
     extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [];
   };
-
-  # Install Hyprland
-  programs.hyprland = {
-    # Install the packages from nixpkgs
-    enable = true;
-    # Whether to enable XWayland # TODO Why did I enable this ???
-    xwayland.enable = true;
-    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
-    # make sure to also set the portal package, so that they are in sync
-    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
-  };
-
-  # Install firefox.
-  programs.firefox.enable = true;
-
-  # Install zsh
-  programs = {
-      zsh = {
-        enable = true;
-        ohMyZsh = {
-          enable = true;
-          theme = "robbyrussell";
-          plugins = [
-            "sudo"
-            "terraform"
-            "systemadmin"
-            "vi-mode"
-          ];
-        };
-      };
-  };
-  users.defaultUserShell = pkgs.zsh;
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
@@ -119,76 +68,18 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    # BEGIN WM
-    glib
-    mpv
-    brightnessctl
-    #light
-    procps
-    inotify-tools
-    acpid
-    acpi
-    #brightnessctl
-    #pulseaudio
-    #hyprexpo
-
-    cmake
-    meson
-    cpio
-
-    htop
-    iproute2
-    jq
-    killall
-    libsecret
-    pciutils
-    podman-compose
-    pipx
-    python312
-    nodejs
-    ruff
-    socat
-    tmux
-    vscodium
-    wget
-    unzip
-    yarn
-    # TODO put it in hardware.config
-    vulkan-loader
-    vulkan-tools
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  wget
+     git
+     # TODO in home.nix
+     kitty
+     firefox
   ];
 
-  #fonts = {
-  #  enableDefaultFonts = true;  # Optional: Ensures basic system fonts are available
-  #  fonts = with pkgs; [
-  #    google-fonts
-  #    nerdfonts
-  #  ];
-  #};
-
-  # Install Steam
-  programs.steam = {
+  programs.hyprland = {
     enable = true;
-    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
   };
-
-  # Install podman
-  # Enable common container config files in /etc/containers
-  virtualisation.containers.enable = true;
-  virtualisation = {
-    podman = {
-      enable = true;
-
-      # Create a `docker` alias for podman, to use it as a drop-in replacement
-      #dockerCompat = true;
-
-      # Required for containers under podman-compose to be able to talk to each other.
-      defaultNetwork.settings.dns_enabled = true;
-    };
-  };
-
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
@@ -198,7 +89,6 @@
   # };
 
   # List services that you want to enable:
-  services.acpid.enable = true;
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
--- home.nix
+++ home.nix
@@ -1,87 +1,32 @@
-{ inputs, config, pkgs, ... }:
-
-  #dotfiles = builtins.fetchGit {
-  #  url = "https://github.com/sylflo/dotfiles.git";
-  #  ref = "main"; # Replace with the desired branch or tag
-  #};
-
-let 
- dotfiles = "/home/sylflo/Projects/dotfiles";
-in
+{ lib, inputs, config, pkgs, ... }:
 {
-
+  
   home.username = "sylflo";
   home.homeDirectory = "/home/sylflo";
 
-  home.packages = with pkgs; [
-    git
-    spotify
-    rofi-wayland
-    swww
-    waypaper
-    ## Window manager
-    eww
-    #hyprlock
-    grim
-    slurp
-    kitty
-    playerctl
-    swaynotificationcenter
-    pipewire
-    wireplumber
-    dolphin # TODO to change/customize
-  ];
 
-  #dotfiles = "/home/sylflo/Projects/dotfiles";
-  ###################
-  # Start Dotfiles  #
-  ###################
-  home.file.".config/hypr".source = "${dotfiles}/config/hypr";
-  home.file.".config/hypr".recursive = true;
-  home.file.".config/hypr".force = true;
-  
-  home.file.".config/kitty".source = "${dotfiles}/config/kitty";
-  home.file.".config/kitty".recursive = true;
-
-  home.file.".config/eww".source = "${dotfiles}/config/eww";
-  home.file.".config/eww".recursive = true;
-
-  home.file.".config/rofi".source = "${dotfiles}/config/rofi";
-  home.file.".config/rofi".recursive = true;
-
-  ################
-  # End dotfiles #
-  ################
-
-  programs.git = {
+  wayland.windowManager.hyprland = {
     enable = true;
-    userName = "sylflo";
-    userEmail = "git@sylvain-chateau.com";
+    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+    plugins = [
+      inputs.hyprland-virtual-desktops.packages.${pkgs.system}.virtual-desktops
+    ];
+    extraConfig = ''
+      #source = ~/.config/hypr/hyprland-source.conf
+      stickyrule = class:^(kittysticky)$,3
+      stickyrule = title:thunderbird,mail
+
+      plugin {
+        virtual-desktops {
+          names = 1:coding, 2:internet, 3:mail and chats 
+          cycleworkspaces = 1
+          rememberlayout = size
+          notifyinit = 0
+          verbose_logging = 0
+        }
+     }
+    '';
   };
-
-
-  # Enable Zsh as the shell
-  programs.zsh = { enable = true; oh-my-zsh = {
-      enable = true; # Install and manage Oh My Zsh
-      plugins = [ "git" "z" "vi-mode" ]; # Add desired plugins
-      theme = "agnoster"; # Set your desired theme
-    };
-  };
-
-  # Manage your .zshrc configuration
-  home.file.".zshrc".text = ''
-    # Custom .zshrc configuration
-    export PATH=$HOME/.local/bin:$HOME/.config/elenapan/bin:$PATH
-
-    # Enable aliases
-    alias ll='ls -la'
-    alias gs='git status'
-
-    # Source oh-my-zsh
-    if [ -f $HOME/.oh-my-zsh/oh-my-zsh.sh ]; then
-      source $HOME/.oh-my-zsh/oh-my-zsh.sh
-    fi
-  '';
 
   # This value determines the home Manager release that your
   # configuration is compatible with. This helps avoid breakage
