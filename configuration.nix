88e3628f5890b9fd7298df02fbf135dd6a0590ce
sylflo <git@sylvain-chateau.com>
2025-01-10 01:04:04 +0100

    f

--- configuration.nix
+++ configuration.nix
@@ -71,9 +71,6 @@
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
      git
-     # TODO in home.nix
-     kitty
-     firefox
   ];
 
   programs.hyprland = {
--- home.nix
+++ home.nix
@@ -4,6 +4,22 @@
   home.username = "sylflo";
   home.homeDirectory = "/home/sylflo";
 
+  home.packages = with pkgs; [
+    firefox
+    spotify
+    rofi-wayland
+    swww
+    waypaper
+    eww
+    #hyprlock # TODO use flake cos of compatbility with hyprland
+    grim
+    slurp
+    kitty
+    playerctl
+    pipewire
+    wireplumber
+    #dolphin # TODO to change/customize
+  ];
 
   wayland.windowManager.hyprland = {
     enable = true;
@@ -27,6 +43,36 @@
      }
     '';
   };
+
+  programs.git = {
+    enable = true;
+    userName = "sylflo";
+    userEmail = "git@sylvain-chateau.com";
+  };
+
+
+  # Enable Zsh as the shell
+  programs.zsh = { enable = true; oh-my-zsh = {
+      enable = true; # Install and manage Oh My Zsh
+      plugins = [ "git" "z" "vi-mode" ]; # Add desired plugins
+      theme = "agnoster"; # Set your desired theme
+    };
+  };
+
+  # Manage your .zshrc configuration
+  home.file.".zshrc".text = ''
+    # Custom .zshrc configuration
+    export PATH=$HOME/.local/bin:$HOME/.config/elenapan/bin:$PATH
+
+    # Enable aliases
+    alias ll='ls -la'
+    alias gs='git status'
+
+    # Source oh-my-zsh
+    if [ -f $HOME/.oh-my-zsh/oh-my-zsh.sh ]; then
+      source $HOME/.oh-my-zsh/oh-my-zsh.sh
+    fi
+  '';
 
   # This value determines the home Manager release that your
   # configuration is compatible with. This helps avoid breakage
