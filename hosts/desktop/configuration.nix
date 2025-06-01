de522ba6db28fd7feb371d31f014c57400d6672c
sylflo <git@sylvain-chateau.com>
2025-06-01 13:18:12 +0200

    add steam for desktop

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -11,4 +11,11 @@
     ];
 
   networking.hostName = "desktop"; # Define your hostname.
+
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = false; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = false; # Open ports in the firewall for Source Dedicated Server
+    localNetworkGameTransfers.openFirewall = false; # Open ports in the firewall for Steam Local Network Game Transfers
+  };
 }
--- hosts/desktop/home.nix
+++ hosts/desktop/home.nix
@@ -4,8 +4,13 @@
     ../../common/home.nix
   ];
 
-  home.packages = lib.mkMerge [
-    (lib.mkDefault [ ])
-    [ pkgs.steam ]
-  ];
+  # home.packages = lib.mkMerge [
+  #   (lib.mkDefault [ ])
+  #   [
+  #     pkgs.steam
+  #     pkgs.vulkan-loader
+  #     pkgs.vulkan-validation-layers
+  #     pkgs.vulkan-extension-layer
+  #   ]
+  # ];
 }
